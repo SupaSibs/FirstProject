8275215a97b14c7be54ba19efeffdca85c35ffed
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 14:00:57 +0200

    mongodb stuff

--- controllers/landing.js
+++ controllers/landing.js
@@ -4,6 +4,13 @@ exports.get_landing =(req, res, next) => {
 res.render('index', { title: 'My First Backend App' } ) }
 
 exports.submit_lead = (req, res, next) => {
-	console.log("lead email:", req.body.lead_email)
+let email = req.body.lead_email;
+console.log("lead email:", email)
 res.redirect('/')
 }
+
+exports.get_lead = (req, res, next) => {
+let email = req.body.lead_email;
+}
+
+module.exports = req.body.lead_email;
--- routes/database.js
+++ routes/database.js
@@ -1,13 +1,19 @@
-//mongoose db
+//mongoose db, express.js and router settings
+let express = require("express")
+let router = express.Router();
 let mongoose = require("mongoose");
+//gets lead email to add to the database
+router.get("/", landing.get_lead);
 //connects to mongoose and uses promises
 mongoose.connect("mongodb://localhost:27017/Learning-Node")
-.then(() => { db = 
-mongoose.connetion;
+.then(() => {
+db = mongoose.connection;
 console.log("Connection successful.")
 })
+
 .catch(() => alert("Connection to Database failed. Please try again later."));
 
 //checks if db exists, if it does it adds the email to the db
 if (db) {
+db.add(email);
 }
