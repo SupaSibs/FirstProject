98f3f809c13de5895dc5f4f1c0645a8be3d63189
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 14:43:12 +0200

    STOP THIS SO MANY THINGS

---
+++ sentry.js
@@ -0,0 +1,31 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://354417e37d824440961296356682f4ae@o1110058.ingest.sentry.io/6138709",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
