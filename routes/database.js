a62327c00caf10ee361ddbd5ddb2bfea34903a5a
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 13:47:18 +0200

    aaaa stop giving me 1k+ files

--- routes/database.js
+++ routes/database.js
@@ -1,3 +1,13 @@
+//mongoose db
 let mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Learning-Node");
-let db = mongoose.connection;
+//connects to mongoose and uses promises
+mongoose.connect("mongodb://localhost:27017/Learning-Node")
+.then(() => { db = 
+mongoose.connetion;
+console.log("Connection successful.")
+})
+.catch(() => alert("Connection to Database failed. Please try again later."));
+
+//checks if db exists, if it does it adds the email to the db
+if (db) {
+}
