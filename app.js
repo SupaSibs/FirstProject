4ed220d75df392fd1354f2e58631a1a520bc703b
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 20:29:20 +0200

    is

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-})); 
+var app = express(); 
 app.post('/', (req, res) => { res.json(req.body) })
 
 // view engine setup
@@ -24,13 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(body-parser())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use((req, res, next) => {
-res.setHeader("Content-Type", "application/json");
-next();
-});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- controllers/landing.js
+++ controllers/landing.js
@@ -2,8 +2,6 @@ exports.get_landing =(req, res, next) => {
 res.render('index', { title: 'My First Backend App' } ) }
 
 exports.submit_lead = (req, res, next) => {
-req.setHeader("Content-Type", "application/json")
-res.setHeader("Content-Type", "application/json")
 let email = req.body.lead_email;
 console.log("lead email:", email);
 return email;
