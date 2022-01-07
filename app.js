60389da37a7d4dd1b85fab1c5872ffdad0da6510
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 20:33:42 +0200

    ss

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express(); 
-app.post('/', (req, res) => { res.json(req.body) })
+app.post('/', (req, res) => { res.json(reqrbody) })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(express.cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
