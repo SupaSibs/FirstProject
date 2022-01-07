e203ef19009c25dc0d9866fd1c4e999558629381
SupaSibs <omareeto2012@hotmail.com>
2022-01-07 12:27:51 +0200

    e

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,4 +1,9 @@
 
 
-exports.get_landing = function(req, res, next) {
+exports.get_landing =(req, res, next) => {
 res.render('index', { title: 'My First Backend App' } ) }
+
+exports.submit_lead = (req, res, next) => {
+	console.log("lead email:", req.body.lead_email)
+res.redirect('/')
+}
