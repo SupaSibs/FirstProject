bd1e21f4b078bcbe07ce70054e2c614f558d9d0c
SupaSibs <omareeto2012@hotmail.com>
2022-01-08 11:05:46 +0200

    br

--- app.js
+++ app.js
@@ -6,23 +6,22 @@ var logger = require('morgan');
 let connect = require("connect")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-let bodyParser = require("body-parser")
+let bodyParser = require("body-parser");
+var cors=require('cors')
 var app = express(); 
+app.use(cors())
+app.use(bodyParser.json({ type:"application/json" }))
 app.post('/', (req, res) => { res.json(reqrbody) })
-
+app.use('routes/', router)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-bodyParser();
-next();	
-});
-app.use((res, req, next) => {
 cookieParser();
-next;
+next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
